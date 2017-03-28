12c57909eddb9f1caf2a8125de7fccb5870c7702
Bartek Kus <Bartekus@users.noreply.github.com>
2017-03-27 20:28:13 -0700

    Array ver (#1)
    * array version
    
    * array version
    
    * Autobots assemble
    
    '

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import Combat from './classes/combat';
 
 const contenders = [
-  ['Soundwave', 'D', 8, 9, 2, 6, 7, 5, 6, 10],
-  ['Bluestreak','A', 6, 6, 7, 9, 5, 2, 9,  7],
-  ['Hubcap',    'A', 4, 4, 4, 4, 4, 4, 4,  4]
+  ['Soundwave',     'D',  8,  9,  2,  6,  7,  5,  6,  10],
+  ['Bluestreak',    'A',  6,  6,  7,  9,  5,  2,  9,   7],
+  ['Hubcap',        'A',  4,  4,  4,  4,  4,  4,  4,   4],
+  ['Predaking',     'D', 10, 10, 10, 10, 10, 10, 10,  10],
+  // ['Optimus Prime', 'A', 10, 10, 10, 10, 10, 10, 10,  10]
 ];
 
 new Combat(contenders);
